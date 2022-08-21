606d2ee81495e788daf1a6144cd2e210120938e8
Vignesh2706 <111271309+Vignesh2706@users.noreply.github.com>
2022-08-21 00:18:01 -0700

    Create inheritance.py

---
+++ inheritance.py
@@ -0,0 +1,39 @@
+class Robot:
+    def __init__(self, name,function,manufacture_year):
+        self.name = name
+        self.function = function
+        self.manufacture_year = manufacture_year
+
+    
+    def say_hi(self):
+        print("Hi, I am " + self.name)
+
+class perfomance:
+    def __init__(self,memory,speed):
+        self.memory = memory
+        self.speed = speed
+
+    def memory_test(self):
+        if self.memory < 10:
+            return "fast"
+        elif self.memory > 20:
+            return "low"
+        else:
+            return "average"
+        
+class PhysicianRobot(Robot):
+    def __init__(self,name,function,manufacture_year,use):
+       super().__init__(name,function,manufacture_year)
+       self.use = use
+
+class AIROBOT(Robot,perfomance):
+    def __init__(self,name,function,manufacture_year,memory,speed):
+        Robot.__init__(self, name,function,manufacture_year)
+        perfomance.__init__(self,memory,speed)
+       
+airobot = AIROBOT("hari","work",2016,128,10)
+
+print(airobot.memory)
+print(airobot.function)
+print(airobot.speed)
+
